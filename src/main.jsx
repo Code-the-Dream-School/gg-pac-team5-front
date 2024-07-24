20e9ca87925dd53ce6ff7e8f19aba5bd55100f50
Jeffrey Cheung <CHEUNG.K.Jeffrey@gmail.com>
2024-07-24 13:30:44 -0700

    Enable mocking for dev environment

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,20 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+async function enableMocking() {
+  if (process.env.NODE_ENV !== "development") {
+    return;
+  }
+
+  const { worker } = await import("./mocks/browser");
+
+  return worker.start();
+}
+
+enableMocking().then(() => {
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+});
