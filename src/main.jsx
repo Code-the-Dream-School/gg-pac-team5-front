b2b3ac922e3592dc8a5df5066b5e49d05fdb8174
Jeffrey Cheung <CHEUNG.K.Jeffrey@gmail.com>
2024-07-23 23:18:52 -0700

    Replace App.jsx with Pages RouterProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
+import Pages from './routes/Pages'
+import ErrorPage from './ErrorPage'
+
+const router = createBrowserRouter([
+  {
+    path: '/pages',
+    element: <Pages />,
+    errorElement: <ErrorPage />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
