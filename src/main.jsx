5e5858ea3b7490fa879c944153d56732c2256748
Jeffrey Cheung <CHEUNG.K.Jeffrey@gmail.com>
2024-07-24 11:40:35 -0700

    Format code

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import './index.css'
-import Pages from './routes/Pages'
-import ErrorPage from './ErrorPage'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Pages from "./routes/Pages";
+import ErrorPage from "./ErrorPage";
 
 const router = createBrowserRouter([
   {
-    path: '/pages/:pageName',
+    path: "/pages/:pageName",
     element: <Pages />,
-    errorElement: <ErrorPage />
-  }
+    errorElement: <ErrorPage />,
+  },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
