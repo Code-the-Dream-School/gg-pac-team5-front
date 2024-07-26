40e4c3bcf02c7fb1186e6b66b8d19fa64dd48b6f
Jeffrey Cheung <CHEUNG.K.Jeffrey@gmail.com>
2024-07-26 13:00:45 -0700

    Revert "Add NavBar to Pages.jsx"
    This reverts commit 2f5eba35f267fa49199a1eab63a76d788863e24f.
    
    The NavBar will be included as all children of the route

--- src/pages/Pages/Pages.jsx
+++ src/pages/Pages/Pages.jsx
@@ -1,11 +1,9 @@
 import { useParams } from "react-router-dom";
-import { NavBar } from "../../Layouts/Header/NavBar";
 
 export default function Pages() {
   const { pageName } = useParams();
   return (
     <>
-      <NavBar />
       <div>
         <h1>{pageName}</h1>
       </div>
