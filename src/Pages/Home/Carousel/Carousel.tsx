2c0e60b32791e3dbc18cef2b418ffcc2a7508809
Pisotski <vlad.pisotski@gmail.com>
2024-07-30 13:12:04 -0700

    carosel is working. Without slowing downon hover. There is an unresolved issue for resizing container with absolute element in it. and calculating gap between cards hiccup. Gap property is removed for now.

--- src/Pages/Home/Carousel/Carousel.tsx
+++ src/Pages/Home/Carousel/Carousel.tsx
@@ -1,28 +1,67 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
+import { motion, useMotionValue, animate } from "framer-motion";
+import { Card } from "./Card";
 import "../../../Assets/Home/Carousel.css";
+import useMeasure from "react-use-measure";
 
-type Snippet = number;
-type List = Snippet[];
+type Card = number;
+type List = Card[];
 
 interface CarouselProps {
 	list: List;
 }
 
 const Carousel: FC<CarouselProps> = ({ list = [] }) => {
+	let [ref, { width }] = useMeasure();
+	// gap must be calculated along
+	// with item size to rerender the component
+	// keeping one source of truth to avoid future bugs
+	const xTranslation = useMotionValue(0);
+
+	// [useMotionValue] where you need to animate properties dynamically
+	// based on user interactions or other factors.
+
+	useEffect(() => {
+		let controls;
+		let finalPosition = -width / 2;
+
+		controls = animate(xTranslation, [0, finalPosition], {
+			ease: "linear",
+			duration: 25,
+			repeat: Infinity,
+			repeatType: "loop",
+			repeatDelay: 0,
+		});
+
+		// cleans up after rerendering
+		return controls.stop;
+	}, [xTranslation, width]);
+
 	return (
-		<div className="carousel-wrapper">
-			{list.length === 0 ? (
-				<> nothing to display </>
-			) : (
-				list.map((item) => {
-					return (
-						<div key={item} className="carousel-item">
-							{item}
-						</div>
-					);
-				})
-			)}
-		</div>
+		// ref={ref} - to measure the element
+		<section aria-labelledby="carousel-heading" className="carousel-wrapper">
+			<h2 id="carousel-heading" className="carousel-heading">
+				SAY HI TO YOUR BEAUTY SPECIALIST
+			</h2>
+			{/* there is a hiccup when component rerenders. 
+			it's because the calculation does not include temp borders  */}
+			<motion.div
+				className="carousel-container"
+				style={{ x: xTranslation }}
+				ref={ref}
+			>
+				{list.length === 0 ? (
+					<> nothing to display </>
+				) : (
+					// the whole component will be rerendered
+					// when the first item of the second list will hit
+					// _the position_ of the first item of the first list on the screen to ensure infinite scrolling
+					[...list, ...list].map((item, idx) => {
+						return <Card key={`${item}-${idx}`} content={item} />;
+					})
+				)}
+			</motion.div>
+		</section>
 	);
 };
 export { Carousel };
