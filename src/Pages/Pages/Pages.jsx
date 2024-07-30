03090d74ab4b0507b649713c44a8def3956564c2
Jeffrey Cheung <CHEUNG.K.Jeffrey@gmail.com>
2024-07-29 19:10:50 -0700

    Rename folder from "pages" to "Pages"

--- src/Pages/Pages/Pages.jsx
+++ src/Pages/Pages/Pages.jsx
@@ -2,13 +2,13 @@ import { useParams } from "react-router-dom";
 import { NavBar } from "../../Layouts/Header/NavBar/NavBar";
 
 export default function Pages() {
-  const { pageName } = useParams();
-  return (
-    <>
-      <NavBar />
-      <div>
-        <h1>{pageName}</h1>
-      </div>
-    </>
-  );
+	const { pageName } = useParams();
+	return (
+		<>
+			<NavBar />
+			<div>
+				<h1>{pageName}</h1>
+			</div>
+		</>
+	);
 }
