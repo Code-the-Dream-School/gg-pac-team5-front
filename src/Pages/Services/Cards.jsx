9a3ff2d6671a226b1880f82aad08adb88e667249
AnujaBujurge29 <anuja.bujurge29@gmail.com>
2024-07-30 09:41:29 -0500

    Added static boxes, serach bar to Page, need to link it with Databse

---
+++ src/Pages/Services/Cards.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Image1 from '../../Assets/Services/salon-service-1.jpg'
+
+const Cards = () => {
+    return (
+        <Card sx={{ maxWidth: 345, m: 2, p: 2, border: 1 }} >
+            <CardMedia
+                sx={{ height: 140 }}
+                image={Image1}
+                title="green iguana"
+            />
+            <CardContent >
+                <Typography gutterBottom variant="h5" component="div">
+                    Service 1
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit
+                    vel sed culpa iste enim ve libero illum odit....
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small">Learn More</Button>
+            </CardActions>
+        </Card>
+    );
+}
+
+export default Cards;
