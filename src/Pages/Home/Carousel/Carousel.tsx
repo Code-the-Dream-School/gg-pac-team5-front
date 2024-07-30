ee87b4ad491f605d5ffe4c52daa7c726e162f197
Pisotski <vlad.pisotski@gmail.com>
2024-07-30 15:04:18 -0700

    overflow issue fixed

--- src/Pages/Home/Carousel/Carousel.tsx
+++ src/Pages/Home/Carousel/Carousel.tsx
@@ -15,21 +15,33 @@ interface CarouselProps {
 const Carousel: FC<CarouselProps> = ({ list = [] }) => {
 	const FAST_DURATION = 25;
 	const SLOW_DURATION = 75;
+
 	const [duration, setDuration] = useState(FAST_DURATION);
 
 	let [ref, { width }] = useMeasure();
 
 	// [useMotionValue] where you need to animate properties dynamically
 	// based on user interactions or other factors.
+
 	const xTranslation = useMotionValue(0);
 	const [mustFinish, setMustFinish] = useState(false);
 	const [rerender, setRerender] = useState(false);
 
 	useEffect(() => {
 		let controls;
+		// the element will move left which means that it's starting point will be moving out of view
+		// final position is to tell how much to the left the element should be shifted before to end the animation cycle
+		// the element has two lists of the same size, meaning that animation must stop when the first list leaves the screen entirely
+		// e.g. width on single list = 200. width of element (2 lists) 2 * 200 = 400
+		// final position is -400 / 2 = -200, that cover the entire list width
 		let finalPosition = -width / 2;
 
+		// this if statement rerenders the component with updates position every time user interacts with it,
+		// so there will be no hiccups and the list wouldn't start from position 0
 		if (mustFinish) {
+			// keeps track of progress
+			// duration has to be less if the list started moving to keep proportional speed
+			// "left over duration"
 			const progressAlreadyMade =
 				duration * (1 - xTranslation.get() / finalPosition);
 			controls = animate(xTranslation, [xTranslation.get(), finalPosition], {
@@ -37,6 +49,10 @@ const Carousel: FC<CarouselProps> = ({ list = [] }) => {
 				duration: progressAlreadyMade,
 				onComplete: () => {
 					setMustFinish(false);
+					// Without this line, the animation may break
+					// because the animate function can sometimes get stuck
+					// or not properly loop through the entire list of items,
+					// especially when the speed changes dynamically.
 					setRerender(!rerender);
 				},
 			});
