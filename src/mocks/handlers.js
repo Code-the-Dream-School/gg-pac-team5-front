19a33e1856885f9a1db42da9d849a08fa2a62422
Jeffrey Cheung <CHEUNG.K.Jeffrey@gmail.com>
2024-07-31 23:55:55 -0700

    Add an example of what it can do

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,1 +1,10 @@
-export const handlers = [];
+import { http } from 'msw'
+ 
+export const handlers = [
+//   // Intercept the "GET /resource" request.
+//   http.get('http://localhost:8000/api/v1/resources', () => {
+//     // And respond with a "text/plain" response
+//     // with a "Hello world!" text response body.
+//     return new Response('Hello world!')
+//   }),
+]
