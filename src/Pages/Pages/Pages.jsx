e55474b86d7abfd2481a4bea1623955b0f58ace6
Jeffrey Cheung <CHEUNG.K.Jeffrey@gmail.com>
2024-08-01 14:15:02 -0700

    Format code

--- src/Pages/Pages/Pages.jsx
+++ src/Pages/Pages/Pages.jsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export default function Pages() {
   const { pageName } = useParams();
@@ -8,7 +8,8 @@ export default function Pages() {
       {/* <NavBar /> */}
       <div>
         <h1>{pageName}</h1>
-      </div><br />
+      </div>
+      <br />
       <Link to="/pages">
         <button type="submit" className="button wide tall luxury">
           Return to Service Page
