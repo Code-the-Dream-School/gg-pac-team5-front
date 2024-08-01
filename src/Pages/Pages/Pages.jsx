0363758478db64a29841ceb05e6b6b24784a1678
Jeffrey Cheung <CHEUNG.K.Jeffrey@gmail.com>
2024-08-01 16:25:42 -0700

    Add data from api fetch

--- src/Pages/Pages/Pages.jsx
+++ src/Pages/Pages/Pages.jsx
@@ -1,13 +1,30 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 
-export default function Pages() {
+export default function Pages(props) {
+  const { vendorId } = { vendorId: "05929396-fea7-4596-9eb1-0faaa2352c59" };
   const { pageName } = useParams();
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(`http://localhost:8000/api/v1/vendors/${vendorId}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isLoading) {
+    return <></>;
+  }
   return (
     <>
       {/* <NavBar /> */}
       <div>
-        <h1>{pageName}</h1>
+        <h1>{data.name}</h1>
       </div>
       <br />
       <Link to="/pages">
