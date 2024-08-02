22d7bfb0c40e996d72e9fcdd86320a2a9bfc05db
Jeffrey Cheung <CHEUNG.K.Jeffrey@gmail.com>
2024-08-02 15:26:48 -0700

    Add working NavLink to registration page

--- src/Pages/Auth/Login.jsx
+++ src/Pages/Auth/Login.jsx
@@ -12,6 +12,7 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { NavLink } from "react-router-dom";
 
 function Copyright(props) {
   return (
@@ -108,9 +109,9 @@ export default function Login() {
                 </Link>
               </Grid>
               <Grid item>
-                <Link href="#" variant="body2">
+                <NavLink to="/auth/register" variant="body2">
                   {"Don't have an account? Sign Up"}
-                </Link>
+                </NavLink>
               </Grid>
             </Grid>
           </Box>
