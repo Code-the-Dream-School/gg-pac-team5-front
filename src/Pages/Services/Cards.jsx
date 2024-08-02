c777bc0375ea9e457af34f0033b2e5b3c0a6ba29
AnujaBujurge29 <anuja.bujurge29@gmail.com>
2024-08-02 11:15:08 -0500

    diplay of cards for services page more responsive

--- src/Pages/Services/Cards.jsx
+++ src/Pages/Services/Cards.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
+
 import { Link } from 'react-router-dom';
+import React from 'react';
 import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
-// import FormLabel from '@mui/material/FormLabel';
+import Button from '@mui/material/Button';
 import Services from './Services';
-import Image1 from '../../Assets/Services/salon-service-1.jpg'
+import Image1 from '../../Assets/Services/salon-service-1.jpg';
 import { FaSearch } from "react-icons/fa";
 import "./Services.css";
 
@@ -28,22 +29,31 @@ const ServicesSample = [
         details: 'cccc ccccc cccc cc ccccc cc ccc ccc ccc cccccc ccc',
         image: Image1,
         Newroute: '/pages/Service_3'
+    },
+    {
+        title: 'Service 4',
+        details: 'cccc ccccc cccc cc ccccc cc ccc ccc ccc cccccc ccc',
+        image: Image1,
+        Newroute: '/pages/Service_3'
+    },
+    {
+        title: 'Service 4',
+        details: 'cccc ccccc cccc cc ccccc cc ccc ccc ccc cccccc ccc',
+        image: Image1,
+        Newroute: '/pages/Service_3'
     }
-]
+    // Add more cards as needed
+];
+
+const onSearchClick = () => alert('Finding Vendors');
 
 const Cards = () => {
     return (
         <div>
-            <h1>Give yourself a little pampering </h1><br />
+            <h1>Give yourself a little pampering</h1><br />
             <h3>Relax, sit back... We promise your best look!!</h3><br />
-            {/* <Link to="/booking">
-                <button type="submit" className="button wide tall luxury">
-                    Book Your Appointment
-                </button>
-            </Link> */}
             <h1>Explore our beauty services...</h1><br />
             <FormControl>
-                {/* <FormLabel id="demo-row-radio-buttons-group-label">Gender</FormLabel> */}
                 <RadioGroup
                     row
                     aria-labelledby="demo-row-radio-buttons-group-label"
@@ -52,18 +62,17 @@ const Cards = () => {
                     <FormControlLabel value="female" control={<Radio />} label="Nail Pampering" />
                     <FormControlLabel value="male" control={<Radio />} label="Tail Pampering" />
                     <FormControlLabel value="other" control={<Radio />} label="Both" />
-
                 </RadioGroup>
             </FormControl><br />
             <div className="searchbar-input-wrapper">
                 <FaSearch id="search-icon" />
                 <input
                     placeholder="Type to search..."
-                // value={input}
-                // onChange={(e) => handleChange(e.target.value)}
                 />
+                <Button variant="contained" size="medium" onClick={onSearchClick}>
+                    Search
+                </Button>
             </div>
-
             {ServicesSample.map((service, index) => (
                 <Services
                     key={index}
@@ -73,12 +82,16 @@ const Cards = () => {
                     Newroute={service.Newroute}
                 />
             ))}
+            <Services cards={ServicesSample} />
+            {/* <Button variant="contained" size="medium" path="/">
+                Return to home page
+            </Button> */}
             <Link to="/">
                 <button type="submit" className="button wide tall luxury">
-                    Return to home page
+                    Return to home Page
                 </button>
             </Link>
         </div>
     );
-}
+};
 export default Cards;
--- src/Pages/Services/Services.jsx
+++ src/Pages/Services/Services.jsx
@@ -5,35 +5,36 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 import { useNavigate } from 'react-router-dom';
 
-const Services = ({ title, image, details, Newroute }) => {
-    const navigate = useNavigate();
-    const handleServices = () => {
-        navigate(Newroute);
-    };
-    return (
-        <div>
-            <Card sx={{ maxWidth: 345, m: 2, p: 2, border: 1 }} >
-                <CardMedia
-                    sx={{ height: 140 }}
-                    image={image}
-                    title={title}
-                />
-                <CardContent >
-                    <Typography gutterBottom variant="h5" component="div">
-                        {title}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        {details}
-                    </Typography>
-                </CardContent>
-                <CardActions >
-                    <Button size="small" onClick={handleServices}>Learn More</Button>
-                </CardActions>
-            </Card >
-        </div>
-    );
-}
+const Services = ({ cards = [] }) => {
 
+    const navigate = useNavigate();
+
+    return (
+        <Box sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', gap: 2 }}>
+            {cards.map((card, index) => (
+                <Card key={index} sx={{ width: 'calc(40% - 16px)', m: 1, p: 2, border: 1 }}>
+                    <CardMedia
+                        sx={{ height: 140 }}
+                        image={card.image}
+                        title={card.title}
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            {card.title}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            {card.details}
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button size="small" onClick={() => navigate(card.Newroute)}>Learn More</Button>
+                    </CardActions>
+                </Card>
+            ))}
+        </Box>
+    );
+};
 export default Services;
