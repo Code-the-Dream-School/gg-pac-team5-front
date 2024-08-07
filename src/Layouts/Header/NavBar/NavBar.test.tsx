990ea5595e179e626c5609e638451410f256f943
Pisotski <vlad.pisotski@gmail.com>
2024-08-06 17:11:32 -0700

    home element testing

--- src/Layouts/Header/NavBar/NavBar.test.tsx
+++ src/Layouts/Header/NavBar/NavBar.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import { NavBar } from "./NavBar";
 import { renderWithRouter } from "../../../util/testingUtils.jsx";
 
