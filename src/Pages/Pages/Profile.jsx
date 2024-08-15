0db54be76d93a9f2832682d3ae4de8f11b5b659b
Tianna <140460149+tianna-spears@users.noreply.github.com>
2024-08-16 00:15:59 +0100

    updated profile component route to display in NavBar until AuthProvider is setup

--- src/Pages/Pages/Profile.jsx
+++ src/Pages/Pages/Profile.jsx
@@ -2,20 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from "../../Assets/Profile/Profile.module.css";
 
-
-function Profile() {
+const Profile = () => {
     return (
         <div className={styles.profile}>
-            <h1> Hello! <br/> </h1>
-            <h2> Welcome to your profile!</h2>
+            <h1> Hello! Welcome to your profile.</h1>
             <img src="images/pic1.jpg" alt="Profile Picture"/>
-            <h3><em> Beauty Client</em></h3>
+            <h3><em> Beauty Client</em></h3> <br/>
             <h4> 
                 Your last beauty appointment was on: 
-                <br/>
+                <br/> <br/>
                 Your upcoming beauty appointment is scheduled on:
             </h4>
-
 
             <Link to="/">
                 <button> Click to return to the home page</button>
