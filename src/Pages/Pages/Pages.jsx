f68fd424f5852b8b90352e227411eed3a8a859e3
Jeffrey Cheung <CHEUNG.K.Jeffrey@gmail.com>
2024-08-21 16:57:07 -0700

    Refactor to have seperate code paths for API and MOCK_API

--- src/Pages/Pages/Pages.jsx
+++ src/Pages/Pages/Pages.jsx
@@ -1,15 +1,28 @@
 import { useLoaderData } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { Avatar, Button, Card, CardMedia, Typography, Tabs, Tab } from "@mui/material";
+import { API, MOCK_API } from "../../config"
 
 export const loader = async ({ params }) => {
+  if (import.meta.env.VITE_REACT_MSW) {
+    const res = await fetch(
+      `${MOCK_API}/vendors/name/${params.pageName}`
+    );
+    if (res.status === 404) {
+      throw new Response("Not Found", { status: 404 });
+    }
+    return res.json();
+  }
+
+  // Hack to get vendor name from backend api
   const res = await fetch(
-    `http://localhost:8000/api/v1/vendors/name/${params.pageName}`
+    `${API}/vendors`
   );
   if (res.status === 404) {
     throw new Response("Not Found", { status: 404 });
   }
-  return res.json();
+  const data = await res.json();
+  return data.vendors.find((vendor) => vendor.name === params.pageName);
 }
 
 export const Pages = () => {
@@ -18,11 +31,11 @@ export const Pages = () => {
   return (
     <>
       <Card>
-        <CardMedia image={page.backgroundImage}>
-          <Avatar alt={page.name} src={page.profileImage} />
+        <CardMedia image={import.meta.env.VITE_REACT_MSW ? page.backgroundImage : "http://not-yet-implemented.invalid/backgroundImage.png"}>
+          <Avatar alt={page.name} src={import.meta.env.VITE_REACT_MSW ? page.profileImage : "http://not-yet-implemented.invalid/profileImage.png"} />
         </CardMedia>
         <Typography gutterBottom variant="h4" component="div">{page.name}</Typography>
-        <Typography gutterBottom variant="h6" component="div">{page.summary}</Typography>
+        <Typography gutterBottom variant="h6" component="div">{import.meta.env.VITE_REACT_MSW ? page.summary : "SUMMARY NOT YET IMPLEMENTED"}</Typography>
         <Button variant="contained">Make an appointment</Button>
       </Card>
       <Card>
@@ -32,7 +45,7 @@ export const Pages = () => {
           <Tab label="Services">
           </Tab>
         </Tabs>
-        <Typography gutterBottom variant="body1" component="div">{page.description}</Typography>
+        <Typography gutterBottom variant="body1" component="div">{import.meta.env.VITE_REACT_MSW ? page.description : "DESCRIPTION NOT YET IMPLEMENTED"}</Typography>
       </Card>
       <br />
       <Link to="/pages">
