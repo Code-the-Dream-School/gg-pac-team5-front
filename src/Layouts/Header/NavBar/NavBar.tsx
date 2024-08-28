3759a8be8a25a9fceb6d9d6c8ebfaa1c13682b98
Pisotski <vlad.pisotski@gmail.com>
2024-08-27 19:20:59 -0700

    conflict resolved

--- src/Layouts/Header/NavBar/NavBar.tsx
+++ src/Layouts/Header/NavBar/NavBar.tsx
@@ -17,8 +17,8 @@ import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 
 const NavBar = () => {
-	const { user, logout } = useContext(UserContext)
-
+	const { user, logout } = useContext(UserContext);
+	const users = true;
 	const activeStyles: CSSProperties = {
 		fontWeight: "bold",
 		textDecoration: "underline",
@@ -34,13 +34,8 @@ const NavBar = () => {
 			displayedName: "Find Service",
 			linksTo: "pages",
 		},
-		{
-			displayedName: "Login",
-			linksTo: "auth",
-		},
 	];
 	const [anchorElNav, setAnchorElNav] = useState(null);
-	const [anchorElUser, setAnchorElUser] = useState(null);
 
 	const handleOpenNavMenu = (event) => {
 		setAnchorElNav(event.currentTarget);
@@ -129,42 +124,30 @@ const NavBar = () => {
 							display: { xs: "flex", md: "flex" },
 						}}
 					>
-						{/* TODO:
-							change alt to real user data  */}
-						<NavLink
-							to="profile"
-							end
-							style={({ isActive }) => (isActive ? activeStyles : undefined)}
-						>
-							<Avatar
-								alt="User"
-								src="https://i.etsystatic.com/16421349/r/il/f21def/3144130361/il_fullxfull.3144130361_2i6w.jpg"
-								sx={{ width: 56, height: 56 }}
-							/>
-						</NavLink>
-      {user ? (<>
-        <NavLink 
-				to="profile"
-				style={({ isActive }) => (isActive ? activeStyles : undefined)}
-			>
-				Profile
-			</NavLink>
-
-				<Button
-					type="submit"
-					variant="contained"
-					onClick={logout}
-					sx={{ mt: 3, mb: 2 }}
-				>
-					Logout
-				</Button></>) :
-				<NavLink
-					to="auth"
-					style={({ isActive }) => (isActive ? activeStyles : undefined)}
-				>
-					Login
-				</NavLink>
-			}
+						{user ? (
+							<>
+								<Avatar
+									alt="User"
+									src="https://i.etsystatic.com/16421349/r/il/f21def/3144130361/il_fullxfull.3144130361_2i6w.jpg"
+									sx={{ width: 56, height: 56 }}
+								/>
+								<Button
+									type="submit"
+									variant="contained"
+									onClick={logout}
+									sx={{ mt: 3, mb: 2 }}
+								>
+									Logout
+								</Button>
+							</>
+						) : (
+							<NavLink
+								to="auth"
+								style={({ isActive }) => (isActive ? activeStyles : undefined)}
+							>
+								Login
+							</NavLink>
+						)}
 					</Box>
 				</Toolbar>
 			</Container>
--- src/Pages/Auth/Auth_Layout.jsx
+++ src/Pages/Auth/Auth_Layout.jsx
@@ -1,29 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { useNavigate, Outlet } from "react-router-dom";
-import Login from './Login';
-import { NavBar } from '../../Layouts/Header/NavBar/NavBar.tsx';
+import Login from "./Login";
+import { NavBar } from "../../Layouts/Header/NavBar/NavBar.tsx";
 
 const Auth_Layout = () => {
 	const [isAuthenticated, setIsAuthenticated] = useState(false);
-	const navigate= useNavigate();
+	const navigate = useNavigate();
 
-	const handleLogin= (email,password) => {
-		if (email=== 'aaa@aaa.aaa' && password=== 'A!1aaaaa') {
+	const handleLogin = (email, password) => {
+		if (email === "aaa@aaa.aaa" && password === "A!1aaaaa") {
 			setIsAuthenticated(true);
-			navigate('/profile');
+			navigate("/profile");
 		} else {
-		alert ('Invalid email or password. Please try again.');
+			alert("Invalid email or password. Please try again.");
 		}
 	};
-	
+
 	return (
 		<>
-		<NavBar isAuthenticated={isAuthenticated} /> 
-		{isAuthenticated ? (
-			<Outlet />
-		) : (
-			<Login onLogin={handleLogin} />
-		)}
+			<NavBar isAuthenticated={isAuthenticated} />
+			{isAuthenticated ? <Outlet /> : <Login onLogin={handleLogin} />}
 		</>
 	);
 };
