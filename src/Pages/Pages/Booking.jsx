e25a877d833173c9ef859abfedd35bc7575f16d8
Jeffrey Cheung <CHEUNG.K.Jeffrey@gmail.com>
2024-09-02 10:04:52 -0700

    Remove unused import

--- src/Pages/Pages/Booking.jsx
+++ src/Pages/Pages/Booking.jsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
