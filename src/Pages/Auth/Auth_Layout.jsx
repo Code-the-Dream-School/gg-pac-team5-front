29e25145157b583602892813ea26fea556cf5f6f
Tianna <140460149+tianna-spears@users.noreply.github.com>
2024-09-03 21:56:16 +0100

    updated logic to remove duplicate NavBar on Login page

--- src/Pages/Auth/Auth_Layout.jsx
+++ src/Pages/Auth/Auth_Layout.jsx
@@ -1,31 +1,31 @@
-import React, { useState } from 'react';
-import { useNavigate, Outlet } from "react-router-dom";
+// Auth_Layout.jsx
+import React from 'react';
+import { useNavigate, Outlet } from 'react-router-dom';
 import Login from './Login';
-import { NavBar } from '../../Layouts/Header/NavBar/NavBar.tsx';
+// import { NavBar } from '../../Layouts/Header/NavBar/NavBar.tsx';
 
-const Auth_Layout = () => {
-	const [isAuthenticated, setIsAuthenticated] = useState(false);
-	const navigate= useNavigate();
+const Auth_Layout = ({ isAuthenticated, setIsAuthenticated }) => {
+    const navigate = useNavigate();
 
-	const handleLogin= (email,password) => {
-		if (email=== 'aaa@aaa.aaa' && password=== 'A!1aaaaa') {
-			setIsAuthenticated(true);
-			navigate('/profile');
-		} else {
-		alert ('Invalid email or password. Please try again.');
-		}
-	};
-	
-	return (
-		<>
-		<NavBar isAuthenticated={isAuthenticated} /> 
-		{isAuthenticated ? (
-			<Outlet />
-		) : (
-			<Login onLogin={handleLogin} />
-		)}
-		</>
-	);
+    const handleLogin = (email, password) => {
+        if (email === 'aaa@aaa.aaa' && password === 'A!1aaaaa') {
+            setIsAuthenticated(true);
+            navigate('/profile');
+        } else {
+            alert('Invalid email or password. Please try again.');
+        }
+    };
+
+    return (
+        <>
+            {/* <NavBar isAuthenticated={isAuthenticated} /> NavBar displaying twice, commented this line out to prevent duplicate*/}
+            {isAuthenticated ? (
+                <Outlet />
+            ) : (
+                <Login onLogin={handleLogin} />
+            )}
+        </>
+    );
 };
 
 export { Auth_Layout };
