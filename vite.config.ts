0acc3ccc5ca92a9ddbd954e5810adc279491d8af
Pisotski <vlad.pisotski@gmail.com>
2024-09-10 16:51:21 -0700

    vite config for vercel change

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,28 @@ import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import { configDefaults } from "vitest/config";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [react(), svgr()],
-	test: {
-		/* 
+export default defineConfig(({ command, mode }) => {
+	// Load environment variables based on the mode
+	const env = loadEnv(mode, process.cwd(), "");
+
+	return {
+		// Plugins for Vite
+		plugins: [react(), svgr()],
+
+		// Vitest configuration
+		test: {
+			/* 
 			jsdom: browser simulation
 			globals: access to expect, describe, test variable by default
 		*/
-		environment: "jsdom",
-		globals: true,
-		setupFiles: "./testSetup.js",
-	},
+			environment: "jsdom",
+			globals: true,
+			setupFiles: "./testSetup.js",
+		},
+
+		// Define custom environment variables
+		define: {
+			"import.meta.env.VITE_API_URL": JSON.stringify(process.env.API_URL),
+		},
+	};
 });
