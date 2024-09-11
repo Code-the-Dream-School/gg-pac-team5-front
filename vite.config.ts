27f4583bd497001c8c806aedf2073ecf2798d71e
Pisotski <vlad.pisotski@gmail.com>
2024-09-10 17:12:43 -0700

    vite config for vercel change

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import { configDefaults } from "vitest/config";
